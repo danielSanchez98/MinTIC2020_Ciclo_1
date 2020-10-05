2a49c55b2fcea90fcb95f2892de2100557031777
csierra <csierra@smarthop.co>
2020-10-05 09:32:13 -0500

    Add first version of  HTML code for views and templates management

--- misiontic2020_ciclo1/tienda_virtual/urls.py
+++ misiontic2020_ciclo1/tienda_virtual/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 url_patterns = [
     url(r'^$', views.home, name="inicio"),
-    url(r'^carrito_compras$', views.carrito, name="carrito"),
-    url(r'^historial_compras$', views.historial, name="historial"),
-    url(r'^productos$', views.productos, name="lista_productos"),
-    url(r'^portal_pagos$', views.pagos, name="pagos"),
+    url(r'^carrito_compras/$', views.carrito, name="carrito"),
+    url(r'^historial_compras/$', views.historial, name="historial"),
+    url(r'^productos/$', views.productos, name="lista_productos"),
+    url(r'^portal_pagos/$', views.pagos, name="pagos"),
 ]

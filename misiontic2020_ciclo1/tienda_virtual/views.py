d7d2d3cb0da6e0f25a81969e50df334969ad6e62
csierra <csierra@smarthop.co>
2020-10-05 01:52:59 -0500

    Add list of current webpages follow MVC definition in django

--- misiontic2020_ciclo1/tienda_virtual/views.py
+++ misiontic2020_ciclo1/tienda_virtual/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'tienda_virtual/index.html')
+
+def carrito(request):
+    return render(request, 'tienda_virtual/carrito_compras.html')
+
+def historial(request):
+    return render(request, 'tienda_virtual/historial.html')
+
+def productos(request):
+    return render(request, 'tienda_virtual/lista_productos.html')
+
+def pagos(request):
+    return render(request, 'tienda_virtual/pagar.html')
